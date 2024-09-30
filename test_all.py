7bd06f0c2c30fddaa8ea3991ecaca90f836607db
LuRobot15 <thelukeroberts@gmail.com>
2024-09-30 17:37:52 +0100

    Added test suite to run all tests

---
+++ test_all.py
@@ -0,0 +1,17 @@
+import unittest
+from test_createDict import TestCreateDict
+from test_main import TestMain
+from test_countdownSolver import TestCountdownSolver
+
+def suite():
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    suite.addTest(loader.loadTestsFromTestCase(TestCreateDict))
+    suite.addTest(loader.loadTestsFromTestCase(TestMain))
+    suite.addTest(loader.loadTestsFromTestCase(TestCountdownSolver))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    test_suite = suite()
+    runner.run(test_suite)
