23a23335c529598ace16557e7bcb724d2d99e3f6
LuRobot15 <thelukeroberts@gmail.com>
2024-11-12 14:48:34 +0000

    registered the models with the admin file

--- WebApp/LettersGameAPI/admin.py
+++ WebApp/LettersGameAPI/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Letters, Answers, Words
+
+admin.site.register(Letters)
+admin.site.register(Words)
+admin.site.register(Answers)
