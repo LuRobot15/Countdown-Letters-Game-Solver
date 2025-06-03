9125bdc02a84cdead3c2ac8092f94a9640e7e06a
LuRobot15 <thelukeroberts@gmail.com>
2025-06-03 09:26:53 +0100

    Removed old Web API

--- WebApp/LettersGameAPI/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Letters, Answers, Words
-
-admin.site.register(Letters)
-admin.site.register(Words)
-admin.site.register(Answers)
