8903c68ae79c27acc2a56ed91e090e65ec9decee
LuRobot15 <thelukeroberts@gmail.com>
2025-07-08 11:33:20 +0100

    Added endpoint to check answer

--- API/main.py
+++ API/main.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from fastapi import FastAPI, Query, HTTPException
 
-from LettersGame.CountdownSolver import solve_countdown
+from LettersGame.CountdownSolver import solve_countdown, check_answer
 from LettersGame.CreateDict import load_dict
 
 app = FastAPI()
@@ -18,18 +18,66 @@ async def root():
     return {"message": "This is the root of the letters game server"}
 
 
-@app.get("/answers/get")
-def get_answers(letters: Annotated[str, Query(
-    description="The 9 letters you want to make words from. Can be upper or lower case",
-    min_length=9,
-    max_length=9
-)]):
-
+@app.get("/answers/get/")
+def get_answers(
+    letters: Annotated[
+        str,
+        Query(
+            description="The 9 letters you want to make words from. Can be upper or lower case",
+            min_length=9,
+            max_length=9
+        )
+    ]
+):
     validate_input_is_char_str(letters)
     usable_letters = letters.lower()
     results = solve_countdown(usable_letters, dict)
     results.sort(key=lambda x: x["length"], reverse=True)
     return results
+
+
+@app.get("/answers/check/")
+def check_answer_endpoint(
+    letters: Annotated[
+        str,
+        Query(
+            description="The 9 letters you want to make words from. Can be upper or lower case",
+            min_length=9,
+            max_length=9
+        )
+    ],
+    word: Annotated[
+        str,
+        Query(
+            description="The word to check",
+            min_length=3,
+            max_length=9
+        )
+    ]
+):
+    letters = preprocess_str_inp(letters)
+    word = preprocess_str_inp(word)
+
+    return check_answer(
+        letters=letters,
+        word=word,
+        search_dict=dict
+    )
+
+
+def preprocess_str_inp(s: str) -> str:
+    """
+    Asserts a string is all letters
+    Returns the str in lowercase
+
+    Args:
+        s (str): the input string
+
+    Returns:
+        str: the preprocessed string
+    """
+    validate_input_is_char_str(s)
+    return s.lower()
 
 
 def validate_input_is_char_str(s: str) -> None:
