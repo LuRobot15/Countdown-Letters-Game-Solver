debf0c76480e21962e917b942c4b22910aaff1e1
LuRobot15 <thelukeroberts@gmail.com>
2025-07-08 11:17:47 +0100

    Added assertion that input letters is alpha only

--- API/main.py
+++ API/main.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 
 from LettersGame.CountdownSolver import solve_countdown
 from LettersGame.CreateDict import load_dict
@@ -24,8 +24,26 @@ def get_answers(letters: Annotated[str, Query(
     min_length=9,
     max_length=9
 )]):
-
     usable_letters = letters.lower()
     results = solve_countdown(usable_letters, dict)
     results.sort(key=lambda x: x["length"], reverse=True)
     return results
+
+
+def validate_input_is_char_str(s: str) -> None:
+    """
+    Asserts a string contains only letters,
+    raising a HTTPException if not
+
+    Args:
+        s (str): The input string
+
+    Raises:
+        HTTPException: terminates the request and
+                       informs the client of their error
+    """
+    if not s.isalpha():
+        raise HTTPException(
+            status_code=400,
+            detail="letters must contain letters only"
+        )
